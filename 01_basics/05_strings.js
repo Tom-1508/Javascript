f13ca1d6c397cc4f1243a0dc91c0f40ed4599d70
Tamal Majumdar <tamalcrj2002@gmail.com>
2024-04-10 21:15:28 +0000

    string question practice

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -38,9 +38,17 @@ const url = "https://tamal.com/tamal200%majumdar"
 // *********************************Practice the different methods of string*********************************
 
 const myName = "tamal"
-console.log(myName.anchor("wow")) // invalid
+//console.log(myName.anchor("wow")) // invalid
 
 // .at
 const sentence = "wow, what a handsome guy you are!"
 
-console.log(`she says me that ${sentence} but she couldn't like this part ${sentence.at(sentence[5])}`)
+// console.log(`she says me that ${sentence} but she couldn't like this part ${sentence.at(sentence[5])}`)
+
+// Write a program that takes a user's input and calculates the length of the input string using the length property.
+
+// const yourName = prompt("enter your name: ") // this is not working in node , so figure out how to take user input 
+
+console.log(`the length of the string is ${yourName.length}`);
+
+// Create a program that prompts the user for a string and an index. Then, use the charAt() method to display the character at that index.
