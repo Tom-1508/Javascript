1243c93ebf990c144ab3362dd440e0428b55fbce
Tamal Majumdar <tamalcrj2002@gmail.com>
2024-04-13 11:24:54 +0000

    adding some notes on Array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -24,11 +24,27 @@ const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 // console.log(Array.from("tamal"))
 //console.log(Array.from({name: "hitesh"})) // interesting 
 
+/*
+
+The Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object.
+
+The Array.from() method creates a new array from an array-like or iterable object. The object you're passing to Array.from() needs to have a length property indicating the number of elements it contains or be iterable.
+
+In your example, {name: "hitesh"} is not an array-like object or iterable because it doesn't have a length property, and it's not iterable. Therefore, Array.from() doesn't know how to create an array from it, resulting in an empty array being returned.
+
+*/
+
 // let score1 = 100
 // let score2 = 200
 // let score3 = 300
 
 // console.log(Array.of(score1,score2,score3))
+
+/*
+
+The Array.of() static method creates a new Array instance from a variable number of arguments, regardless of number or type of the arguments.
+
+*/
 
 
 // const Array1 = [1,2,3,4,5];
