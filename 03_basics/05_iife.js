a3d0ff00bc9fb0aa5e5b547c689c425d97a67f3a
Tamal Majumdar <tamalcrj2002@gmail.com>
2024-04-13 21:13:51 +0000

    IIFE coded

--- 03_basics/05_iife.js
+++ 03_basics/05_iife.js
@@ -1,1 +1,21 @@
 //  Immediately Invoked Function Expressions (IIFE)
+// why we use iife? => 
+
+
+// function chai(){
+//     console.log(`DB CONNECTED`);
+// }
+// chai()
+
+(function chai(){ 
+    // Named IIFE
+    console.log(`DB CONNECTED`);
+})(); // semicolon is most important to tell the IIFE that this context should be stopped
+
+// (definition of function) (execution call)
+
+// there are some cases in which global scope pollute things up, so to remove those , we have used IIFE.
+
+( (name) => {
+    console.log(`DB CONNECTED TWO, ${name}`);
+} ) ("Tamal");
