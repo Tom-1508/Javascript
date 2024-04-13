7a2386c5865990315560cb19c21abb9dab3972a1
Tamal Majumdar <tamalcrj2002@gmail.com>
2024-04-13 16:29:38 +0000

    scope and mini hoisting

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -9,12 +9,59 @@ if (true) { // this is block scope
     let a = 10
     const b = 20
     // var c = 30
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 //Global scope of Windows and node are completely different (interview)
+
+
+
+// nested scope
+
+function one () {
+    const userName = "Tamal"
+
+    function two(){
+        const website = "youtube"
+        console.log(userName)
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const userName = "Tamal"
+    if(userName === "Tamal"){
+        const website = " youtube"
+        // console.log(userName+website)
+    }
+    // console.log(website);
+}
+// console.log(userName);
+
+
+// *************************** Interesting **********************
+
+// Hoisting
+
+console.log(addOne(5))
+
+function addOne (num) { // simple function
+    return num + 1
+}
+
+
+
+addTwo(5)
+
+const addTwo = function (num){ //Expression 
+    return num + 2
+}
