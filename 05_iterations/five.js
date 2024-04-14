3d8763fdca5eb19526c80ae3e1bfdc0dbb9c96bb
Tamal Majumdar <tamalcrj2002@gmail.com>
2024-04-14 17:17:14 +0000

    forEach

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,2 +1,43 @@
 const coding = ["js",'rub', "py", "java", "cpp"]
 
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// } )
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe) // function ka reference do, not execution
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// } )
+
+
+const myCoding = [
+    {
+        languageName: "javaScript",
+        languageFile: "js"
+    },
+
+    {
+        languageName: "java",
+        languageFile: "java"
+    },
+
+    {
+        languageName: "python",
+        languageFile: "py"
+    }
+] // most important , most used
+
+// myCoding.forEach( (item) => {
+//     console.log(item);
+//     console.log(item.languageName);
+//     console.log(item.languageFile);
+// } )
